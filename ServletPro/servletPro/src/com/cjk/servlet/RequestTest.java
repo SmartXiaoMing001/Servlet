67facff48c6e195ae2a9252260d0694a15c9d5f4
曹静坤 <caojingkun886@163.com>
2017-08-07 12:34:01 +0800

    daily operation

--- ServletPro/servletPro/src/com/cjk/servlet/RequestTest.java
+++ ServletPro/servletPro/src/com/cjk/servlet/RequestTest.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 public class RequestTest extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
